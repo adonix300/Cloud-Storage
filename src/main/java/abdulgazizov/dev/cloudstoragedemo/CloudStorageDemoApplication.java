39452e4b5d04394a427608f17cfde32340c2511f
adonix300 <businessskyjoyboy@gmail.com>
2024-04-24 14:21:34 +0500

    Redis adding

--- src/main/java/abdulgazizov/dev/cloudstoragedemo/CloudStorageDemoApplication.java
+++ src/main/java/abdulgazizov/dev/cloudstoragedemo/CloudStorageDemoApplication.java
@@ -2,8 +2,12 @@ package abdulgazizov.dev.cloudstoragedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching
 public class CloudStorageDemoApplication {
 
     public static void main(String[] args) {
