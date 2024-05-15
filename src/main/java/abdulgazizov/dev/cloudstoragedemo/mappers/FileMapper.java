5c55efe27ec713c7ae5562014bf04edbba822efc
adonix300 <businessskyjoyboy@gmail.com>
2024-05-15 09:00:31 +0500

    created

---
+++ src/main/java/abdulgazizov/dev/cloudstoragedemo/mappers/FileMapper.java
@@ -0,0 +1,26 @@
+package abdulgazizov.dev.cloudstoragedemo.mappers;
+
+import abdulgazizov.dev.cloudstoragedemo.dtos.FileDto;
+import io.minio.messages.Item;
+
+import java.time.ZonedDateTime;
+
+public class FileMapper {
+
+    public static FileDto toFileDto(Item fileItem) {
+        ZonedDateTime lastModified = fileItem.lastModified();
+        return FileDto.builder()
+                .fileName(fileItem.objectName())
+                .size(fileItem.size())
+                .fileType(determineFileType(fileItem.objectName()))
+                .editedAt(lastModified.toInstant().toEpochMilli())
+                .build();
+    }
+
+    private static String determineFileType(String fileName) {
+        if (fileName.contains(".")) {
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        }
+        return "unknown";
+    }
+}
